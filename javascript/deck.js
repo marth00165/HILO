b87daa9e7bf8edbeb805266ef77290a8be1cbd97
Spencer Lindemuth <lindemut@gmail.com>
2019-07-29 10:36:54 -0700

    copypasta

--- javascript/deck.js
+++ javascript/deck.js
@@ -3,6 +3,12 @@ class Deck {
     }
 
     get freshDeck() {
-           return  [{faceValue: "2C", url: "../images/cards/2C.png" }]
+           return  [{faceValue: "2C", url: "../images/cards/2C.png" },
+                    {faceValue: "2C", url: "../images/cards/2C.png" },
+                    {faceValue: "2C", url: "../images/cards/2C.png" },
+                    {faceValue: "2C", url: "../images/cards/2C.png" },
+                    {faceValue: "2C", url: "../images/cards/2C.png" },
+                    {faceValue: "2C", url: "../images/cards/2C.png" },
+                    {faceValue: "2C", url: "../images/cards/2C.png" },]
     }
 }
