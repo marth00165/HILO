afe5f24077fb81e953ed8f73b892da8cc0a92895
Rohit <rohitpratti@gmail.com>
2019-07-31 16:43:10 -0700

    philip error fix

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,20 @@ class GamesController < ApplicationController
     gameRender = @games.map{|game| newGame = {id: game.id, user_id: game.user_id, guesses: game.guesses}}
     render json: gameRender, except: [:created_at, :updated_at]
   end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game, except: [:created_at, :updated_at]
+  end
+
+  def new
+    @game = Game.new(params.require(:game).permit(:user_id, :guesses)
+  end
+
+  def create
+    @game = Game.new(params.require(:game).permit(:user_id, :guesses))
+    @user = User.find(params[:game][:user_id])
+    render json: @user, include: {:games}, except: [:created_at, :updated_at]
+
+  end
 end
