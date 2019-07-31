9df3b866c60ae6d764abbcdf958ae0e56a96d728
Rohit <rohitpratti@gmail.com>
2019-07-31 15:55:49 -0700

    deployfam

--- db/migrate/20190729170316_create_games.rb
+++ db/migrate/20190729170316_create_games.rb
@@ -1,8 +1,8 @@
 class CreateGames < ActiveRecord::Migration[5.2]
   def change
     create_table :games do |t|
-      t.string :winner
-
+      t.integer :user_id
+      t.integer :guesses
       t.timestamps
     end
   end
