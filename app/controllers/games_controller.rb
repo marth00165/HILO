e0744a9b87efec7b4fad3d4cf93dd7e1ab845663
Rohit <rohitpratti@gmail.com>
2019-07-31 16:13:33 -0700

    back-end games updated

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,7 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+    gameRender = @games.map{|game| newGame = {id: game.id, user_id: game.user_id, guesses: game.guesses}}
+    render json: gameRender, except: [:created_at, :updated_at]
+  end
 end
